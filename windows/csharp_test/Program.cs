ec23f54a5692f76fb6c6165ec7a6e89ed0566358
Junyi Hu <Accelouch@gmail.com>
2017-02-23 00:07:06 +0800

    Fixed EmguCV input bug

--- windows/csharp_test/Program.cs
+++ windows/csharp_test/Program.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CaffeSharp;
+using Emgu.CV;
+using Emgu.CV.Structure;
 using FastDetection;
 
 namespace csharp_test
@@ -24,7 +26,7 @@ namespace csharp_test
 
             FastFace ff=new FastFace(1.1f,3,24);
 
-            Bitmap bmp=new Bitmap(@"D:\Research\FacialLandmarks\Data\UMD\umdfaces_batch2\stan_freberg\stan_freberg_0001.jpg");
+            Bitmap bmp=new Bitmap(@"D:\Research\FacialLandmarks\Data\UMD\umdfaces_batch2\stan_freberg\stan_freberg_0013.jpg");
 
 
             FaceInfo info = ff.Facedetect_Multiview_Reinforce(bmp);
@@ -32,23 +34,37 @@ namespace csharp_test
             Bitmap[] bits = new Bitmap[info.count];
             for (int i = 0; i < info.count; i++)
             {
-                int StartX = Convert.ToInt32(info.r[i].X - info.r[i].Width*0.2);
-                int StartY = Convert.ToInt32(info.r[i].Y - info.r[i].Height * 0.2);
-                int iWidth = Convert.ToInt32(info.r[i].Width * 1.4);
-                int iHeight = Convert.ToInt32(info.r[i].Height * 1.4);
-                bits[i] = KiCut(bmp, StartX, StartY, iWidth, iHeight);
+                //int StartX = Convert.ToInt32(info.r[i].X - info.r[i].Width*0.2);
+                //int StartY = Convert.ToInt32(info.r[i].Y - info.r[i].Height * 0.2);
+                //int iWidth = Convert.ToInt32(info.r[i].Width * 1.4);
+                //int iHeight = Convert.ToInt32(info.r[i].Height * 1.4);
+                //bits[i] = KiCut(bmp, StartX, StartY, iWidth, iHeight);
+
+                Image<Bgr, byte> image = new Image<Bgr, byte>(bmp);
+                Rectangle rect = info.r[i];
+                rect.X = Convert.ToInt32(rect.X - rect.Width * 0.2);
+                rect.Y = Convert.ToInt32(rect.Y - rect.Height * 0.2);
+                rect.Width = Convert.ToInt32(rect.Width * 1.4);
+                rect.Height = Convert.ToInt32(rect.Height * 1.4);
+                image.ROI = rect;
+                bits[i] = image.Bitmap;
+                image.ROI = Rectangle.Empty; ;
             }
-            float[][] aa = IPBbox.ExtractBitmapOutputs(bits, new[] { "fc2", "fc3" }, 0);
-            Rectangle[] rects = new Rectangle[info.count];
-            Bitmap[] C = CaffeModel.Align_Step1(bits, rects, aa[0], aa[1]);
+            if (info.count>0)
+            {
+                float[][] aa = IPBbox.ExtractBitmapOutputs(bits, new[] { "fc2", "fc3" }, 0);
+                Rectangle[] rects = new Rectangle[info.count];
+                Bitmap[] C = CaffeModel.Align_Step1(bits, rects, aa[0], aa[1]);
 
-            float[] bb = IPTs5.ExtractBitmapOutputs(C, "fc2", 0);
-            Bitmap[] F = CaffeModel.Align_Step2(bits, C, bb, rects, 128, 128);
+                float[] bb = IPTs5.ExtractBitmapOutputs(C, "fc2", 0);
+                Bitmap[] F = CaffeModel.Align_Step2(bits, C, bb, rects, 128, 128);
 
-            F[0].Save("test.jpg");
-            float[][] a =
-            IPBbox.ExtractFileOutputs(new[] {@"D:\Research\FacialLandmarks\Data\5PTS\batch1_1.jpg"},
-                new[] {"fc2", "fc3"}, 0);
+                F[0].Save("test.jpg");
+                float[][] a =
+                IPBbox.ExtractFileOutputs(new[] { @"D:\Research\FacialLandmarks\Data\5PTS\batch1_1.jpg" },
+                    new[] { "fc2", "fc3" }, 0);
+            }
+            
         }
 
         public static Bitmap KiCut(Bitmap b, int StartX, int StartY, int iWidth, int iHeight)
